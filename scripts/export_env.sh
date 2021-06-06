d42752539bca1227844347ca343ba8a9855b62d0
cardistack <cardistack@protonmail.com>
2021-06-06 23:31:58 +0100

    Added Alonzo_blue config files

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -1,7 +1,7 @@
 # Source this with the CONTEXT and NETWORK env set, containing the full path and
 # supported network name.
 # e.g.
-# CONTEXT=$PWD NETWORK=mainnet testnet or mary_qa etc  scripts/export_env.sh
+# CONTEXT=$PWD NETWORK=mainnet testnet or mary_qa, alonzo_blue etc  scripts/export_env.sh
 
 export BIN_DIR=${CONTEXT}/bin
 export CONFIG_DIR=${CONTEXT}/config/network/${NETWORK}
@@ -21,6 +21,11 @@ case "$NETWORK" in
             RELAY_PORT=3005
             BP_PORT=3006
             SUBNET=192.168.35
+            ;;
+        alonzo_blue)
+            RELAY_PORT=3007
+            BP_PORT=3008
+            SUBNET=192.168.36
             ;;
 esac
 
