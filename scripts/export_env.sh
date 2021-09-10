a4cbdcc00b7369b3487683ad9937686eddbb7f81
cardistack <cardistack@protonmail.com>
2021-09-10 18:08:35 +0100

    Updated Mainnet and Testnet config files to Alonzo. Updated path to docker images to node 1.29.0

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -15,7 +15,7 @@ case "$NETWORK" in
         testnet)
             RELAY_PORT=3003
             BP_PORT=3004
-            SUBNET=192.168.34   
+            SUBNET=192.168.34
             ;;
         mary_qa)
             RELAY_PORT=3005
@@ -26,6 +26,11 @@ case "$NETWORK" in
             RELAY_PORT=3007
             BP_PORT=3008
             SUBNET=192.168.36
+            ;;
+         alonzo)
+            RELAY_PORT=3008
+            BP_PORT=3009
+            SUBNET=192.168.37
             ;;
 esac
 
